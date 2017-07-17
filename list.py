87ea858d3443eb98f7fde27619f724bbe99d66e4
snoooer <snoooer@outlook.com>
2017-07-17 05:46:56 -0500

    Add files via upload

--- list.py
+++ list.py
@@ -1,17 +1,25 @@
 #!/usr/bin/env python
 
-letters = ['a','b','c','d','e','f']
+cheeses = ['Cheddra','Edam','Gouda']
 
-letters.append('h')
-print(letters)
-a = ['a','b','c']
-n = [1,2,3]
-x = [a,n]
+numbers = [43,123]
+
+empty = []
+
+def only_upper(t):
+    res = []
+    for s in t:
+        if s.isupper():
+            res.append(s)
+    return res
+def bad_del_head(t)
+    
+t = ['a','b','c','E','e','f']
+x = t.pop(2)
 print(x)
-print(x[0])
-print(x[1][2])
-
-x1,x2 = 0,1
-while x2 < 10:
-    print(x2,end=',')
-    x1,x2 = x2,x1 + x2
+del t[0]
+t.remove('e')
+s = 'Python is good'
+s1 = s.split()
+t1 = ' '.join(s1)
+print(t1)
